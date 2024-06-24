f81fcde03ebb497837c00876caf49316ce567ec3
Александр <c260187@yahoo.com>
2024-06-24 19:18:43 +0300

    Add files via upload

---
+++ resolve2.py
@@ -0,0 +1,32 @@
+# Задача № 1
+print('a')
+print(len('a'))
+a = 1
+# Задача № 2
+first = 5
+second = 10
+print(first + second)
+print(second - first)
+summa = 15
+diff = 5
+print(15)
+print(5)
+# Задача № 3
+first = 6
+second = 8
+third = 4
+print((6 + 8 + 4)// 3)
+mean = 6
+print(6)
+# Задача№4
+first_string = 'Ворник'
+second_string = 'Понедельник'
+print('Понедельник' ', ' 'Вторник')
+# Задача № 5
+a = 16
+b = 7
+c = 9
+print((a*b)+(a*c))
+f = 256
+print((f ** 3) / 2)
+print(8388608.0)
