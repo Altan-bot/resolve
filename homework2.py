9b82ff4b6eead84e5c253f9958672813c522b17c
Александр <c260187@yahoo.com>
2024-06-27 00:08:45 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+#переменные
+home_work=12
+total_time=1.5
+course_name='Python'
+print(total_time/home_work)
+average_time=0.125
+print('Курс:',course_name,',всего задач:',home_work,',затрачено часов:'
+      ,total_time,',среднее время выполнения:',average_time,'часа.')
