dd4c68a8cf2b758b3eb44650a33bfa7d794780d0
Александр <c260187@yahoo.com>
2024-06-30 08:03:15 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -2,7 +2,5 @@
 home_work=12
 total_time=1.5
 course_name='Python'
-print(total_time/home_work)
-average_time=0.125
-print('Курс:',course_name,',всего задач:',home_work,',затрачено часов:'
-      ,total_time,',среднее время выполнения:',average_time,'часа.')
+print('Курс:',course_name,', всего задач:',home_work,', затрачено часов:'
+      ,total_time,', среднее время выполнения:',total_time/home_work,'часа.')
