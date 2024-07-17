1986501207c450ddb3f7aedd83ee68305e6af7d1
Александр <c260187@yahoo.com>
2024-07-17 10:19:11 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input('Ведите имя: ')
+print('Здравствуйте,',my_string)
+my_string='Urban'
+print(my_string[2:4])
+print(my_string.upper())
+print(my_string.lower())
+print('Urban','urban', sep='')
+print(my_string[0])
+print(my_string[-1])
