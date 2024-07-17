d5e5ec936c12802cbe0355d1a107fe48718002b8
Александр <c260187@yahoo.com>
2024-07-17 14:34:05 +0300

    Add files via upload

--- homework6.py
+++ homework6.py
@@ -1,6 +1,7 @@
 my_dict={'Александр': 1987,'Анатолий': 1986}
 print(my_dict)
 print(my_dict['Александр'])
+my_dict['Алексей']=1990
 my_dict.update({'Евгиний': 2000,'Сергей': 1995})
 print(my_dict)
 del my_dict['Сергей']
