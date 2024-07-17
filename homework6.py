dfa45625ad3513cd01cb96bb431a344ef9f2242e
Александр <c260187@yahoo.com>
2024-07-17 13:49:25 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,13 @@
+my_dict={'Александр': 1987,'Анатолий': 1986}
+print(my_dict)
+print(my_dict['Александр'])
+my_dict.update({'Евгиний': 2000,'Сергей': 1995})
+print(my_dict)
+del my_dict['Сергей']
+print(my_dict)
+my_set={5,8,7,3,8,3,5}
+print(my_set)
+print(my_set.add(1,))
+print(my_set)
+print(my_set.discard(8))
+print(my_set)
