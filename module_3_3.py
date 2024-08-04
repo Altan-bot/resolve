6aee97301b7beaa44cdbadc58df1a18e7124766f
Александр <c260187@yahoo.com>
2024-08-04 07:46:48 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [5, 'Alex', False]
+values_dict = {'a': 6, "b": "wall", 'c': False}
+print_params(*values_list)
+print_params(**values_dict)
+
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
