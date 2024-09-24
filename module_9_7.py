739c7111794130f92868ae93ddfe666f23b750f1
Александр <c260187@yahoo.com>
2024-09-24 18:36:08 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,20 @@
+def is_prime(func):
+    def wrapper(*args):
+        result = func(*args)
+        for i in range(2, int(result ** 0.5) + 1):
+            if result % i == 0:
+                print('Составное')
+                break
+        else:
+            print('Простое')
+        return result
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+result = sum_three(2, 3, 6)
+print(result)
